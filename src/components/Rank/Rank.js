6423b77f4b95d589073875895c05d7263a5bdd83
anacarolinasmith <oliveiradesouza@g.harvard.edu>
2018-05-09 18:56:39 -0400

    adding front end

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div>
+			<div className='white f3 pa3'>
+				{'Ana, your current rank is...'}
+			</div>
+			<div className='f1'>
+				{'#5'}
+			</div>
+		</div>
+	);
+}
+
+export default Rank;
